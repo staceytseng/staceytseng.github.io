ded0e34e38d473a43e870d6d329214a0d33059bc
Fred Meng <fredmeng@fmba.local>
2021-08-18 21:38:38 +1000

    update data

--- pamir-highway/data.js
+++ pamir-highway/data.js
@@ -1,6 +1,7 @@
 const coords = [
 [40.53449,72.80836,"Osh"],
 [39.64879,72.89192,"Sary-Mogol"],
+[39.3847331,73.3227539,"Kyzylart Pass (Kyrgyzstan/Tajikistan border)"],
 [39.0104466,73.556385,"Karakul"],
 [38.20248,74.02224,"Murghab"],
 [37.75026,73.2579,"Alichur"],
